ad3ea972efcab57618d278618c17ac0b016b3dff
Aslaha Nasreen <aslahanasreen2018@gmail.com>
2025-01-01 00:05:12 +0530

    fourth

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = express()
 server.use(cors())
 server.use(express.json())
 server.use(route)
-server.use('/uploads',express.static('./Uploads'))
+server.use('/uploads',express.static('./uploads'))
 
 const PORT = 3000 || process.env.PORT
 
